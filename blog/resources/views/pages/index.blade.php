f1f66fa50953acb0a4b98d5e5df4c9c092880c0d
Julia <jul.fischinger.18@lehre.mosbach.dhbw.de>
2019-11-13 18:54:31 +0100

    Kleine Änderungen

--- blog/resources/views/pages/index.blade.php
+++ blog/resources/views/pages/index.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
+
 @section('content')
 <div class="container">
-    <div class="card">
-        <div class="card-header">Startseite</div>
-        <div class="card-body">
-            Das ist die Startseite
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h2>Wilkommen bei <strong>RezeptBuch!</h2>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    Du hast dich erfolgreich eingeloggt.
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
